04777f1969af2dc795d698d8df4cf4c5f4962065
Kiryl Dubarenka <k.duborenko@gmail.com>
2016-11-30 20:34:45 -0800

    Update logs download and analyze scripts

--- src/main/script/download_logs.sh
+++ src/main/script/download_logs.sh
@@ -2,7 +2,7 @@
 
 docker ps | tail -n +2 | while read container image _; do
     echo "Downloading logs from $container[$image]"
-    ip=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' $container`
+    ip=`docker inspect --format '{{ .NetworkSettings.Networks.workshop.IPAddress }}' $container`
     mkdir -p target/logs
     docker logs $container > target/logs/$ip.${image##*\/}.log.txt
 done
